f6fd17335ed44938d7aadb02dcab81a7c2665b89
Naresh Upadhyay <naresh.king88898@gmail.com>
2019-07-24 23:41:30 +0530

    Updates

---
+++ precache-manifest.24536bd74c951f9c3994495eceb9b997.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb54b78c0868abee173319e2268c59d",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriend/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "32f21372b2684a485d6d",
+    "url": "/robofriend/static/css/main.a986554c.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriend/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "32f21372b2684a485d6d",
+    "url": "/robofriend/static/js/main.a84b694c.chunk.js"
+  },
+  {
+    "revision": "75cc8018399ebb82c64f",
+    "url": "/robofriend/static/js/runtime~main.f5f06c82.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
